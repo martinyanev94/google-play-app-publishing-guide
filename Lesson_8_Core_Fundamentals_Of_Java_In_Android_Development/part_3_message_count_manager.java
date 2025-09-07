2ab0886e5641062670d31c63b7c242502e8c483f
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:19:34 -0400

    Add Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_3_message_count_manager.java

---
+++ Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_3_message_count_manager.java
@@ -0,0 +1,5 @@
+int totalMessages = 5;
+int newMessages = 3;
+int allMessages = totalMessages + newMessages; // adds both variables
+totalMessages++; // Increments by 1
+newMessages--; // Decrements by 1

8ad2d959baec275825ea21cd2feb5ed3e6d9158e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:17:01 -0400

    Add Lesson_19_Understanding_Fragments_In_Android_Development/part_3_SimpleFragment.java

---
+++ Lesson_19_Understanding_Fragments_In_Android_Development/part_3_SimpleFragment.java
@@ -0,0 +1,8 @@
+public class SimpleFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_simple, container, false);
+    }
+}

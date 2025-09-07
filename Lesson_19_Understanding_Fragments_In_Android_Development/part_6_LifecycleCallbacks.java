60794863ca9ab94ab32d34d35a747a206be0bd68
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:17:05 -0400

    Add Lesson_19_Understanding_Fragments_In_Android_Development/part_6_LifecycleCallbacks.java

---
+++ Lesson_19_Understanding_Fragments_In_Android_Development/part_6_LifecycleCallbacks.java
@@ -0,0 +1,11 @@
+@Override
+public void onStart() {
+    super.onStart();
+    // Fetch data or start animations here
+}
+
+@Override
+public void onStop() {
+    super.onStop();
+    // Release resources to prevent memory leaks
+}

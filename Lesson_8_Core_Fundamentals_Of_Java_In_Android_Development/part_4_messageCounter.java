0e77663e127371d0c8343e4f2ff6bde7d4fb293e
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:19:35 -0400

    Add Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_4_messageCounter.java

---
+++ Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_4_messageCounter.java
@@ -0,0 +1,8 @@
+if (unreadMessages > 0) {
+    Log.i("Message", "You have unread messages!");
+} else {
+    Log.i("Message", "No new messages.");
+}
+for (int i = 0; i < 5; i++) {
+    Log.i("Counter", "Count is: " + i);
+}

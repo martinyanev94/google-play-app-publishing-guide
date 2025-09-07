9818abb3544973b17729a9cd112ea310ebf23f7b
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:19:48 -0400

    Add Lesson_9_Comprehensive_Guide_To_Java_Methods/part_6_DisplayMethods.java

---
+++ Lesson_9_Comprehensive_Guide_To_Java_Methods/part_6_DisplayMethods.java
@@ -0,0 +1,9 @@
+public void display(int number) {
+    System.out.println("Number: " + number);
+}
+
+public void display(String text) {
+    System.out.println("Text: " + text);
+}
+display(10); // Calls the first method
+display("Hello"); // Calls the second method

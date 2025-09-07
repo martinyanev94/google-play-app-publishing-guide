9579e87659cdce8e63949205f7169276ac4b2050
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:17:42 -0400

    Add Lesson_23_Capturing_Images_In_Android_Applications/part_1_CaptureImageActivity.java

---
+++ Lesson_23_Capturing_Images_In_Android_Applications/part_1_CaptureImageActivity.java
@@ -0,0 +1,9 @@
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+        Bundle extras = data.getExtras();
+        Bitmap imageBitmap = (Bitmap) extras.get("data");
+        imageView.setImageBitmap(imageBitmap);
+    }
+}

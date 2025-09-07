ae7d1ae32e19de8e63d60717639d855c58fe6d31
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:17:40 -0400

    Add Lesson_23_Capturing_Images_In_Android_Applications/part_0_dispatchTakePicture.java

---
+++ Lesson_23_Capturing_Images_In_Android_Applications/part_0_dispatchTakePicture.java
@@ -0,0 +1,6 @@
+private void dispatchTakePictureIntent() {
+    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    }
+}

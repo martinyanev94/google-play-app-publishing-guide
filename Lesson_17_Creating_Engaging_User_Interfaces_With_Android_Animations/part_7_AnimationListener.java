c8a400ed5aa2842f2af5b751893cf192f1b95c44
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:16:53 -0400

    Add Lesson_17_Creating_Engaging_User_Interfaces_With_Android_Animations/part_7_AnimationListener.java

---
+++ Lesson_17_Creating_Engaging_User_Interfaces_With_Android_Animations/part_7_AnimationListener.java
@@ -0,0 +1,16 @@
+animFadeIn.setAnimationListener(new Animation.AnimationListener() {
+    @Override
+    public void onAnimationStart(Animation animation) {
+        // Code to execute when the animation starts
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        // Code to execute when the animation ends
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+        // Code to execute when the animation repeats
+    }
+});

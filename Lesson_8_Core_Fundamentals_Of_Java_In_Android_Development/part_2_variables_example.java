1e80078ab62cbaafb657a16898e97edd32d622dc
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2025-09-06 21:19:32 -0400

    Add Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_2_variables_example.java

---
+++ Lesson_8_Core_Fundamentals_Of_Java_In_Android_Development/part_2_variables_example.java
@@ -0,0 +1,5 @@
+int unreadMessages = 0; // Whole number
+float accountBalance = 129.99f; // Floating-point number
+boolean isNewUser = true; // True or false
+char firstInitial = 'A'; // A single character
+String userName = "Alice"; // A string of characters
